778cca637a437e58c28c810eee5005c645569aef
benjaminvitale <bvitale@udesa.edu.ar>
2025-09-21 19:42:15 -0300

    yes

---
+++ src/main/java/org/udesa/giftcard/model/clock.java
@@ -0,0 +1,14 @@
+package java.org.udesa.giftcard.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Clock {
+    public LocalDate today() {
+        return LocalDate.now();
+    }
+
+    public LocalDateTime now() {
+        return LocalDateTime.now();
+    }
+}
